dd012de1746a5d6d861093e8b0989b12ee799f5f
wuguozhang <lich.wu2014@gmail.com>
2024-03-14 18:06:38 +0800

    update column

--- server/model/invitecode/invite_code.go
+++ server/model/invitecode/invite_code.go
@@ -8,7 +8,7 @@ import (
 // inviteCode表 结构体  InviteCode
 type InviteCode struct {
 	global.GVA_MODEL
-	Code        string `json:"code" form:"code" gorm:"column:code;comment:邀请码;size:32;"`                           //邀请码
+	Code        string `json:"code" form:"code" gorm:"column:code;comment:邀请码;size:32;unique;"`                           //邀请码
 	CreatorUuid string `json:"creatorUuid" form:"creatorUuid" gorm:"column:creator_uuid;comment:创建人uuid;size:64;"` //创建人uuid
 	NewUuid     string `json:"newUuid" form:"newUuid" gorm:"column:new_uuid;comment:使用者uuid;size:64;"`             //使用者uuid
 	FreeData    *int   `json:"freeData" form:"freeData" gorm:"column:free_data;comment:;type:INT;"`                //freeData字段
